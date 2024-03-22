944d6535b0da1f795df10f91653c56d24c05e2f9
PriyankaGiridharan123 <priyankariya963@gmail.com>
2024-03-22 08:43:23 +0530

    Day 6

---
+++ jobspark/src/main/java/com/example/jobspark/JobsparkApplication.java
@@ -0,0 +1,13 @@
+package com.example.jobspark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobsparkApplication.class, args);
+	}
+
+}
---
+++ jobspark/src/test/java/com/example/jobspark/JobsparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jobspark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobsparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
